8138753cb4774575f713ace192efbbaccc073ed6
wakame009 <sayuri@sayurinombp.elecom>
2024-01-16 00:21:20 +0900

    utorialDevelop

---
+++ src/test/java/com/techacademy/controller/SpringTest.java
@@ -0,0 +1,93 @@
+package com.techacademy.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.structurizr.configuration.User;
+import com.techacademy.service.UserService;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class SpringTest {
+
+    //@Test
+    //void test() {
+        //fail("まだ実装されていません");
+    
+    @WebMvcTest(UserController.class)
+    public class UserControllerTests {
+
+        @Autowired
+        private MockMvc mockMvc;
+
+        @BeforeEach
+        void beforeEach(WebApplicationContext webApplicationContext) {
+            // Spring Securityを有効にする
+            mockMvc = MockMvcBuilders
+                    .webAppContextSetup(webApplicationContext)
+                    .apply(springSecurity()).build();
+        }
+
+        @MockBean
+        private UserService userService;
+
+        @Test
+        public void testGetList() throws Exception {
+
+
+            List<com.techacademy.entity.User> userList = null;
+            // UserServiceのモック設定
+            when(userService.getUserList()).thenReturn(userList);
+
+            // コントローラーの動作をテスト
+            mockMvc.perform(MockMvcRequestBuilders.get("/user/list"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.view().name("user/list"))
+                    .andExpect(model().attributeExists("userlist"))
+                    .andExpect(model().attribute("userlist", userList))
+                    .andExpect(model().attributeDoesNotExist("error"));
+
+            // Modelのテスト
+            Model model = (Model) mockMvc.perform(MockMvcRequestBuilders.get("/user/list")).andReturn().getModelAndView().getModel();
+            @SuppressWarnings("unchecked")
+            List<User> retrievedUserList = (List<User>) model.getAttribute("userlist");
+
+            // 件数が3件であること
+            assert retrievedUserList != null;
+            assert retrievedUserList.size() == 3;
+
+            // userlistから1件ずつ取り出し、idとnameを検証する
+            for (int i = 0; i < 3; i++) {
+
+
+            }
+        }
+    }
+    }
+
+    
+    
+    
+    
+
--- src/test/java/com/techacademy/controller/UserControllerTest.java
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -1,29 +1,16 @@
 package com.techacademy.controller;
 
-import static org.assertj.core.api.Assertions.allOf;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import java.util.Arrays;
-import java.util.List;
-import com.techacademy.entity.User;
-import com.techacademy.service.UserService;
-
-import org.hamcrest.Matcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -40,15 +27,6 @@ import com.techacademy.entity.User;
 @ExtendWith(SpringExtension.class)
 class UserControllerTest {
     private MockMvc mockMvc;
-    @Mock
-    private UserService userService;
-    @InjectMocks
-    private UserController userController;
-    @BeforeEach
-    void beforeEach1() {
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-    }
-
 
     private final WebApplicationContext webApplicationContext;
 
@@ -67,7 +45,7 @@ class UserControllerTest {
     @Test
     @DisplayName("User更新画面")
     @WithMockUser
-    void testGetUser(User user, Object User) throws Exception {
+    void testGetUser() throws Exception {
         // HTTPリクエストに対するレスポンスの検証
         MvcResult result = mockMvc.perform(get("/user/update/1/")) // URLにアクセス
             .andExpect(status().isOk()) // ステータスを確認
@@ -75,42 +53,11 @@ class UserControllerTest {
             .andExpect(model().hasNoErrors()) // Modelのエラー有無の確認
             .andExpect(view().name("user/update")) // viewの確認
             .andReturn(); // 内容の取得
-        Object user1 = null;
-        Object user2 = null;
-        Object user3 = null;
-        List<Object> userList = Arrays.asList(user1, user2, user3);
-        
-     // モックの設定
-        List<User> userList1 = 
-        (List<com.techacademy.entity.User>) when(userService.getUserList());
-
-        // HTTPリクエストに対するレスポンスの検証
-        MvcResult result1 = mockMvc.perform(get("/user/list"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("userlist"))
-                .andExpect(model().hasNoErrors())
-                .andExpect(model().attribute("userlist", userList))
-                .andExpect(model().attribute("userlist", hasSize(3)))
-                .andExpect(model().attribute("userlist", hasItem(user1)))
-                .andExpect(model().attribute("userlist", hasItem(user2)))
-                .andExpect(model().attribute("userlist", hasItem(user3)))
-                .andExpect(view().name("user/list"))
-                .andReturn();
-                             
-                
 
         // userの検証
         // Modelからuserを取り出す
-        
-    }
-
-    private Object hasProperty(String string, Matcher<String> matcher) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
-    }
-
-    private Object hasSize(int i) {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+        User user = (User)result.getModelAndView().getModel().get("user");
+        assertEquals(user.getId(), 1);
+        assertEquals(user.getName(), "キラメキ太郎");
     }
 }
